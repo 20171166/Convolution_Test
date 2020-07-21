c9d336156fdb027e79753c48623eb3d5b2ad194c
Seung won <laurent01@unist.ac.kr>
2020-07-21 10:54:27 +0900

    Initial Commit

---
+++ Convolution_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Convolution_Test
+//
+//  Created by Seung won Yoo on 2020/07/21.
+//  Copyright © 2020 Seung won Yoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
